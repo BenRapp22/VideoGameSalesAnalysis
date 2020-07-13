096f75c285ffd5cc17a6d6b702d7c7624ed7e354
BenRapp22 <ben.rapp22@gmail.com>
2020-07-13 16:48:03 -0400

    End of day 1 (7/13) - Created bar graph comparing genre and global sales - Learned how to use the reset index function after grouping/agging
    Still need to:
    - Learn how to label graph axes in plotly
    - Learn how to more easily integrate df's into plotly figures

--- DataExploration.py
+++ DataExploration.py
@@ -1,9 +1,19 @@
 import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+
 
 file = "C:/Users/Benra/OneDrive/Documents/VideoGameSales/vgsales.csv"
 
 df = pd.read_csv(file)
 
 df
+gs = df[['Genre', 'Global_Sales']].groupby('Genre').agg('sum')
+gs.reset_index(level=0, inplace=True)
 
-shape = df.shape
+g = gs['Genre']
+s = gs['Global_Sales']
+
+fig = px.bar(x=g, y=s, color=g, labels={'Genre':'Global Sales (in millions)'})
+fig.show()
